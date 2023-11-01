6c5ec561cab17aacc68d2b9e233529c414385573
mmanni27 <144376910+mmanni27@users.noreply.github.com>
2023-11-01 16:22:45 -0700

    data-joining.sql

---
+++ data-joining.sql
@@ -0,0 +1,27 @@
+-- combine all cleaned datasets into one table with Union All
+
+CREATE TABLE `bike-share-case-study-mm.bike_data.combined_ride_data` 
+AS
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2022_10`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2022_11`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2022_12`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_01`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_02`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_03`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_04`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_05`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_06`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_07`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_08`
+UNION ALL
+SELECT * FROM `bike-share-case-study-mm.bike_data.cleaned_2023_09`;
